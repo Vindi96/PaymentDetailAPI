368a6d6d841f85e4f8758e416552f4d1da2277ad
Vindi96 <vindya.madhushani96@gmail.com>
2021-05-01 09:39:46 +0530

    first commit

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public String CardOwnerName { get; set; }
+        [Column(TypeName = "nvarchar(16)")]
+        public String CardNumber { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public String ExpirationDate { get; set; }
+        [Column(TypeName = "nvarchar(3 )")]
+        public String SecurityCode { get; set; }
+
+    }
+}
